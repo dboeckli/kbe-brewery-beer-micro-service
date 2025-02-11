e2e0980fd33025f882a849f8032f20860f094a50
adm-boeckli <dboeckli@gmail.com>
2025-02-11 13:39:45 +0100

    use hikari and upgrade mysql

--- src/main/java/ch/dboeckli/springframeworkguru/kbe/beer/services/domain/Brewery.java
+++ src/main/java/ch/dboeckli/springframeworkguru/kbe/beer/services/domain/Brewery.java
@@ -39,7 +39,7 @@ import java.util.UUID;
 public class Brewery {
 
     @Id
-    @GeneratedValue(generator = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
     @UuidGenerator
     @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
